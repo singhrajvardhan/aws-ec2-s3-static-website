18fd2bc6860dba44fba1adae56e1e65ce9f95e22
Rajvardhn Singh <rajvardhansinghbadgujar@gmail.com>
2026-01-16 21:22:58 +0530

    Update template.sh

--- template.sh
+++ template.sh
@@ -9,3 +9,4 @@ cd /var/www/html
 git clone https://github.com/atulkamble/aws-ec2-s3-static-website.git
 cp -r aws-ec2-s3-static-website/* /var/www/html/
 echo "<h1>Hello from $(hostname -f) webserver</h1>" >> /var/www/html/index.html
+
